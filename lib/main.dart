969f7c94c041533b60cfcfac644a92e551cf50cb
Viceversa98 <49507281+Viceversa98@users.noreply.github.com>
2021-11-24 23:30:40 +0800

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/material.dart';
+import 'package:worldtimeproject/pages/choose_location.dart';
+import 'package:worldtimeproject/pages/home.dart';
+import 'package:worldtimeproject/pages/loading.dart';
+
+void main() => runApp(MaterialApp(
+
+  initialRoute: '/',
+
+  routes: {
+    '/':(context) => Loading(),
+    '/home': (context)=> Home(),
+    '/location': (context) => ChooseLocation(),
+  },
+
+));
